8f188475585e7e26a75d3149658531b8fb256029
Mur Sukma Pandawa <nursukma2k17@gmail.com>
2026-04-15 20:17:51 +0700

    fix : change

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ export default function Home() {
     <main className="h-dvh w-full overflow-hidden">
       <iframe
         className="h-full w-full border-0"
-        src="https://online.fliphtml5.com/sukmapandawa/skpi/"
-        title="Interactive Wedding Invitation Template"
+        src="https://online.fliphtml5.com/sukmapandawa/wi-q4Nd/"
+        title="Wedding Invitation Sukma & Nia"
         scrolling="no"
         frameBorder="0"
         allowFullScreen
       />
+      {/* <iframe style="position:absolute;border:none;width:100%;height:100%;left:0;top:0;" src="https://online.fliphtml5.com/sukmapandawa/wi-q4Nd/" title="Wedding Invitation Sukma & Nia" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" ></iframe> */}
     </main>
   );
 }
